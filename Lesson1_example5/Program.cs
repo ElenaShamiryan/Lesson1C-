a8baa73fe293b423bfd34ac96c5e1146b65425ff
ElenaShamiryan <elen91@list.ru>
2022-07-07 23:38:11 +0300

    lesson1_example5

---
+++ Lesson1_example5/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "элена")
+{
+    Console.WriteLine("Ура, это же ЭЛЕНА!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+} 
